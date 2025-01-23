15fbc07c11874c9b30617cf4fb72115a2f4c4ead
Daemon Jung <jjjssswww1006@gmail.com>
2025-01-24 04:12:08 +0900

    feature/#4: 구글 위치 라이브러리 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,4 +65,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.timber)
+    implementation(libs.play.services.location)
 }
