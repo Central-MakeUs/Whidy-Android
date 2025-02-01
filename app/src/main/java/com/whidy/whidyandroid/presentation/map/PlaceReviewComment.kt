226b2b514f341d4f771616d36d79855bd27acba9
Daemon Jung <jjjssswww1006@gmail.com>
2025-02-01 17:19:38 +0900

    feature/#4: 장소 정보 후기 UI

---
+++ app/src/main/java/com/whidy/whidyandroid/presentation/map/PlaceReviewComment.kt
@@ -0,0 +1,9 @@
+package com.whidy.whidyandroid.presentation.map
+
+data class PlaceReviewComment(
+    val userProfileImageUrl: String,
+    val userName: String,
+    val reviewScore: Int,
+    val reviewTime: String,
+    val reviewComment: String,
+)
