6d46ebc1cf1e938a6eec166de62c10733e2f6763
Daemon Jung <jjjssswww1006@gmail.com>
2025-03-02 20:38:22 +0900

    feature/#15: 지도가 있는 홈화면에서 뒤로 가기 실행시 불필요한 백스택 없이 앱 나가기

--- app/src/main/java/com/whidy/whidyandroid/presentation/map/home/MapFragment.kt
+++ app/src/main/java/com/whidy/whidyandroid/presentation/map/home/MapFragment.kt
@@ -9,6 +9,7 @@ import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -81,6 +82,12 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         navController = Navigation.findNavController(view)
 
         (requireActivity() as MainActivity).hideBottomNavigation(false)
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                requireActivity().finish()
+            }
+        })
 
         navController.currentBackStackEntry?.savedStateHandle
             ?.getLiveData<Boolean>("showPlaceAddSuccessDialog")
