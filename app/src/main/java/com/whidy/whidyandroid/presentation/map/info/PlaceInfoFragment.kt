99b2a1fc82626af21f60751732c6990737162a21
Daemon Jung <jjjssswww1006@gmail.com>
2025-03-08 00:52:13 +0900

    hotfix/#25: 후기 작성 후 상세정보 화면 이동시 전환 효과 및 별점 후기 갱신

--- app/src/main/java/com/whidy/whidyandroid/presentation/map/info/PlaceInfoFragment.kt
+++ app/src/main/java/com/whidy/whidyandroid/presentation/map/info/PlaceInfoFragment.kt
@@ -10,6 +10,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -329,6 +330,20 @@ class PlaceInfoFragment: Fragment() {
 
         mapViewModel.reviews.observe(viewLifecycleOwner) { reviews ->
             placeReviewCommentAdapter.updateData(reviews)
+
+            mapViewModel.placeDetail.observe(viewLifecycleOwner) { place ->
+                when (place.placeType) {
+                    "GENERAL_CAFE" -> mapViewModel.fetchPlaceGeneralCafe(place.id)
+                    "STUDY_CAFE" -> mapViewModel.fetchPlaceStudyCafe(place.id)
+                    "FREE_STUDY_SPACE" -> mapViewModel.fetchPlaceFreeStudy(place.id)
+                    "FREE_CLOTHES_RENTAL" -> mapViewModel.fetchPlaceFreeClothes(place.id)
+                    "FREE_PICTURE" -> mapViewModel.fetchPlaceFreePicture(place.id)
+                    "FRANCHISE_CAFE" -> mapViewModel.fetchPlaceFranchiseCafe(place.id)
+                    else -> {
+                        Toast.makeText(context, "알 수 없는 장소 타입", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
 
             val keywordCountMap = mutableMapOf<String, Int>()
             reviews.forEach { review ->
